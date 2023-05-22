719afc6fcee8d7684e4fe03ef35f5f3d3b227488
Eliška Vokounová <vokounova.eliska@gmail.com>
2023-05-22 22:28:05 +0200

    first working template

---
+++ app/model/entities/Plant.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Blog\Model\Entities;
+
+use DateTime;
+
+/**
+ * Class Plant
+ * @package Blog\Model\Entities
+ * @property int $plant_id
+ * @property string $name
+ * @property string $latin_name
+ * @property string $description
+ * @property int $owner
+ * @property datetime $bought_date
+ * @property int $water_frequency
+ * @property int $temperature
+ * @property string $lighting
+ * @property string $origin
+ * @property boolean $humidity
+ * @property datetime $last_modified
+ */
+class Plant{
+
+
+  /**
+   * Funkce vracející pole s daty pro ukládání v DB
+   * @return array
+   */
+  public function getDataArr(){
+    $result=[
+       'plant_id' => $this->plant_id,
+        'name' => $this->name,
+        'latin_name' => $this->latin_name,
+        'description' => $this->description,
+        'owner' => $this->owner,
+        'bought_date' => $this->bought_date,
+        'water_frequency' => $this->water_frequency,
+        'temperature' => $this->temperature,
+        'lighting' => $this->lighting,
+        'origin' => $this->origin,
+        'humidity' => $this->humidity,
+        'last_modified' => $this->last_modified
+    ];
+    if (!empty($this->plant_id)){
+      $result['plant_id']=$this->plant_id;
+    }
+    return $result;
+  }
+
+}
